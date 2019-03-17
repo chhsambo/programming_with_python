f3c94e6edd252be2bd54b4c9600d9121c18a93ef
Sambo Chhay <chhaysambo@gmail.com>
2019-03-17 10:00:56 +0700

    Week 6 - Mini Dictionary

--- Code/mini-dictionary/app.py
+++ Code/mini-dictionary/app.py
@@ -1,17 +1,11 @@
-import json
-
-
-def get_definition(search_word):
-    with open("data.json") as data_file:
-        data = data_file.read()
-        data = json.loads(data)
-
-        definition = data[search_word]
-        return search_word, definition
+import utils
 
 
 search_word = input('Enter word: ')
-search_word = search_word.lower()
-word, definition = get_definition(search_word)
+# search_word = search_word.lower()
+definition = utils.get_definition(search_word)
 for d in definition:
     print(f'- {d}')
+
+if not definition:
+    print('Word not found!')
